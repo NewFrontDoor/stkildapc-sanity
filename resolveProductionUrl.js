0986d7705902c717b8fce2b4ab5318dbc4417eea
Bratalix <bratalix@gmail.com>
2021-10-27 14:59:03 +1100

    add prefix to resolve redirect issue

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -1,8 +1,8 @@
-const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET; // ensure this is set in .env before deploying
+const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET; // ensure this is set in .env.production before deploying and .env.development for testing
 const projectUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://stkildapc.org';
+    : 'https://www.stkildapc.org';
 
 
 export default function resolveProductionUrl(document) {
